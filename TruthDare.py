9e7e498f8a9a16457c7f09d466f53ed52261c38f
Darsh-Turakhia-GNU <56975912+Darsh1561@users.noreply.github.com>
2021-06-20 09:45:37 +0530

    Add files via upload

---
+++ TruthDare.py
@@ -0,0 +1,17 @@
+from random import randint
+
+#name of players
+name = ["Mac","Joe","Tim","Bill"]
+
+#number of players
+number = 4
+
+#person asking
+question = name[randint(0,number-1)]
+
+#person answering
+answer = name[randint(0,number-1)]
+
+#print
+print("Question : ",question)
+print("Answer : ",answer)
